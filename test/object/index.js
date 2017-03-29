601a21b353c4a3513895a3ec602a92e8ece91c92
锡炎 <xiyan.xxm@alibaba-inc.com>
2017-03-29 11:31:15 +0800

    feature. add JSON and object encoder.

---
+++ test/object/index.js
@@ -0,0 +1,71 @@
+/* eslint import/no-extraneous-dependencies: 0 */
+import { expect } from 'chai';
+import fixtures from './fixtures';
+import { encoder } from '../../lib/object';
+
+describe('object analysis', () => {
+  it('basic types: boolean', (done) => {
+    const ret = encoder(fixtures.boolean);
+    expect(ret).to.equal(fixtures.boolean);
+    done();
+  });
+
+  it('basic types: number', (done) => {
+    const ret = encoder(fixtures.number);
+    expect(ret).to.equal(fixtures.number);
+    done();
+  });
+
+  it('basic types: null', (done) => {
+    const ret = encoder(fixtures.null);
+    expect(ret).to.equal(fixtures.null);
+    done();
+  });
+
+  it('basic types: string', (done) => {
+    const ret = encoder(fixtures.string);
+    expect(ret).to.equal(fixtures.string);
+    done();
+  });
+
+  it('basic types: function', (done) => {
+    const ret = encoder(fixtures.function);
+    expect(ret).to.equal('[Function]');
+    done();
+  });
+
+  it('string: very long', (done) => {
+    const ret = encoder(fixtures.veryLongString);
+    expect(ret.length).to.equal(49);
+    done();
+  });
+
+  it('object: immutable', (done) => {
+    const ret = encoder(fixtures.object);
+    expect(ret).not.to.equal(fixtures.object);
+    done();
+  });
+
+  it('object: deep copy', (done) => {
+    const obj = fixtures.object;
+    const ret = encoder(obj);
+    expect(ret.boolean).to.equal(obj.boolean);
+    expect(ret.string).to.equal(obj.string);
+    expect(ret.number).to.equal(obj.number);
+    expect(ret.null).to.equal(obj.null);
+    expect(ret.function).to.equal('[Function]');
+    expect(ret.object).not.to.equal(obj.object);
+    expect(ret.object.string).to.equal(obj.object.string);
+    done();
+  });
+
+  it('object: very deep', (done) => {
+    const ret = encoder(fixtures.veryDeepObject);
+    let member = ret;
+    while (member.object) {
+      member = member.object;
+    }
+    expect(member).to.equal('[Object]');
+    done();
+  });
+});
---
+++ webpack/webpack.test.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var nodeExternals = require('webpack-node-externals');
+var WebpackShellPlugin = require('webpack-shell-plugin');
+var path = require('path');
+
+var config = {
+  context: path.join(__dirname, '..', '/test/'),
+  entry: {
+    test: './index.js',
+  },
+  output: {
+    path: path.join(__dirname, '..', '/test_dist'),
+    filename: '[name].js'
+  },
+  target: 'node',
+
+  plugins: [
+    new WebpackShellPlugin({
+      onBuildExit: "mocha test_dist/test.js --slow 5000 --timeout 10000"
+    })
+  ],
+  resolveLoader: {
+    root: path.join(__dirname, '..', '/node_modules'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx'],
+    alias: {}
+  },
+};
+
+module.exports = config;
